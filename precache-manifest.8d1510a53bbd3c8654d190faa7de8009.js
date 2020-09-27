369d686ea467d7c5c9ac6e67666b475011689cf0
Catalin Enache <catalinenache78@gmail.com>
2020-09-27 18:56:26 +0300

    Updates

---
+++ precache-manifest.8d1510a53bbd3c8654d190faa7de8009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3a86022e2f9bce32de8409bf348988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1acbe894cf6fbbbb796b",
+    "url": "/static/css/main.048e888d.chunk.css"
+  },
+  {
+    "revision": "7016e3c54dc4dc4ab319",
+    "url": "/static/js/2.d42ddae3.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.d42ddae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1acbe894cf6fbbbb796b",
+    "url": "/static/js/main.eb6639db.chunk.js"
+  },
+  {
+    "revision": "f47bff7e9953a1e98f70",
+    "url": "/static/js/runtime-main.da034283.js"
+  }
+]);
